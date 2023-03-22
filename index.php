5910b4fd8520ad14bab32f3b685608a4f626767d
Brody14 <stefania.spinas@gmail.com>
2023-03-22 15:06:21 +0100

    FEAT vuejs

--- index.php
+++ index.php
@@ -6,22 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP ToDo List JSON</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 <body>
-    <section>
-			<div class="container pt-5 text-center">
-				<h1>ToDo List</h1>
-			</div>
-			<div class="container py-3">
-				<ul class="list-group">
-					<li class="list-group-item">A disabled item</li>
-					<li class="list-group-item">A second item</li>
-					<li class="list-group-item">A third item</li>
-					<li class="list-group-item">A fourth item</li>
-					<li class="list-group-item">And a fifth one</li>
-				</ul>
-			</div>
-    </section>
+    <div id="app">
+			<section>
+				<div class="container pt-5 text-center">
+					<h1>{{ title }}</h1>
+				</div>
+				<div class="container py-3">
+					<ul class="list-group">
+						<li class="list-group-item">A disabled item</li>
+						<li class="list-group-item">A second item</li>
+						<li class="list-group-item">A third item</li>
+						<li class="list-group-item">A fourth item</li>
+						<li class="list-group-item">And a fifth one</li>
+					</ul>
+				</div>
+			</section>
+		</div>
+
+		<script src="./script.js"></script>
 
 </body>
 </html>
